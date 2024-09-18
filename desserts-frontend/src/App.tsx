763e8497c0bc06ec0a64cb52836d0a76c6c34215
szabo.tamas <szabo.tamas@BURG-1E06-03>
2024-09-18 14:17:45 +0200

    save

---
+++ desserts-frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import DessertCard from './components/DessertCard'
+
+const App = () => {
+  return (
+    <div>
+        <DessertCard name={"Waffle"}/>
+        <DessertCard name={"Macaron Mix"}/>
+        <DessertCard name={"Classic"}/>
+    </div>
+  )
+}
+
+export default App
---
+++ desserts-frontend/src/components/DessertCard.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+type DessertCardProp = {
+    name: string
+}
+
+const DessertCard = (props: DessertCardProp) => {
+  return (
+    <div>
+        {props.name}
+    </div>
+  )
+}
+
+export default DessertCard
